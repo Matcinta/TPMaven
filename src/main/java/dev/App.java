4a40df3366784021b3ee0f94d79cb76ca922c631
Mathilde MUNIER <muniermathilde@gmail.com>
2019-05-31 11:39:46 +0200

    6eme commit - integration librairie tierce

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -4,10 +4,13 @@ package dev;
  * Hello world!
  *
  */
+import com.github.lalyos.jfiglet.FigletFont;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	String asciiArt = FigletFont.convertOneLine("hello John");
+        System.out.println(asciiArt);
     }
 }
